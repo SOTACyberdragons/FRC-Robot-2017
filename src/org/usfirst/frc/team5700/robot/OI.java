a48c7479e0514280a319c95e64d36c32b69faf32
Roman Dimov <romanddimov@gmail.com>
2017-01-05 22:22:50 -0800

    add two joysticks for the driver

--- src/org/usfirst/frc/team5700/robot/OI.java
+++ src/org/usfirst/frc/team5700/robot/OI.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team5700.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Button;
 import org.usfirst.frc.team5700.robot.commands.ExampleCommand;
 
@@ -9,6 +10,9 @@ import org.usfirst.frc.team5700.robot.commands.ExampleCommand;
  */
 
 public class OI {
+	
+	Joystick leftStick;
+	Joystick rightStick;
     //// CREATING BUTTONS
     // One type of button is a joystick button which is any button on a joystick.
     // You create one by telling it which joystick it's on and which button
