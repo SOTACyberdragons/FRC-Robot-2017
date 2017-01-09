25ef8cf99283b46c829cdf2a5c1cccc1a0194908
Roman Dimov <romanddimov@gmail.com>
2017-01-08 18:35:40 -0800

    delete test comment and add commented drive type chooser onto SmartDashboard
    In the main robot class, this adds a sendable chooser to allow the
    driver to pick various drive types, i.e. tank drive, arcade drive, etc.
    It is currently commented out to avoid errors before the commands and
    subsystems are finished.

--- src/org/usfirst/frc/team5700/robot/Robot.java
+++ src/org/usfirst/frc/team5700/robot/Robot.java
@@ -23,7 +23,7 @@ public class Robot extends IterativeRobot {
 	public static OI oi;
 
     Command autonomousCommand;
-    SendableChooser chooser;
+    SendableChooser driveschooser;
 
     /**
      * This function is run when the robot is first started up and should be
@@ -31,10 +31,10 @@ public class Robot extends IterativeRobot {
      */
     public void robotInit() {
 		oi = new OI();
-        chooser = new SendableChooser();
+        drivechooser = new SendableChooser();
         chooser.addDefault("Default Auto", new ExampleCommand());
 //        chooser.addObject("My Auto", new MyAutoCommand());
-        SmartDashboard.putData("Auto mode", chooser);
+        SmartDashboard.putData("Auto mode", drivechooser);
     }
 	
 	/**
