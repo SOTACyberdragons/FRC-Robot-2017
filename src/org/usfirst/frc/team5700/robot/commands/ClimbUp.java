022c7505eb28a615e24f0019091fdba90bfd5bcf
Roman Dimov <romanddimov@gmail.com>
2017-01-15 17:41:53 -0800

    make whileHeld method stop winch when trigger is let go
    I found that the whileHeld method runs the interrupted() method when
    let go instead of the end() method

--- src/org/usfirst/frc/team5700/robot/commands/ClimbUp.java
+++ src/org/usfirst/frc/team5700/robot/commands/ClimbUp.java
@@ -25,7 +25,7 @@ public class ClimbUp extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+    	return false;
     }
 
     // Called once after isFinished returns true
@@ -36,5 +36,6 @@ public class ClimbUp extends Command {
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	Robot.ropeclimber.climb(0);
     }
 }
